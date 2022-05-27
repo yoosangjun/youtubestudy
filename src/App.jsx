a365ee95f6605d337eb185cb888a1c61b0362ea8
yoo sang jun <52834205+yoosangjun@users.noreply.github.com>
2022-05-27 23:12:08 +0900

    0527;

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import "./App.css";
-import React, { useEffect, useState } from "react";
+import styles from "./app.module.css";
+import React, { useEffect, useRef, useState } from "react";
 import VideoList from "./components/video_list/video_list";
+import SearchHeader from "./components/search_header/search_header";
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
 
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&maxResults=25&q=${query}&key=AIzaSyDqHr4xkHDfzqAVXL1gERdf67TlG-VvI9o`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => {
+        setVideos(items);
+        console.log(videos);
+      })
+      .catch((error) => console.log("error", error));
+  };
   useEffect(() => {
     const requestOptions = {
       method: "GET",
@@ -12,7 +32,7 @@ function App() {
     };
 
     fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDqHr4xkHDfzqAVXL1gERdf67TlG-VvI9o",
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&q=&key=AIzaSyDqHr4xkHDfzqAVXL1gERdf67TlG-VvI9o`,
       requestOptions
     )
       .then((response) => response.json())
@@ -24,7 +44,10 @@ function App() {
   }, []);
   return (
     <>
-      <VideoList videos={videos} />
+      <div className={styles.app}>
+        <SearchHeader onSearch={search} />
+        <VideoList videos={videos} />
+      </div>
     </>
   );
 }
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,41 @@
+import React, { useRef } from "react";
+import styles from "./search_header.module.css";
+
+const SearchHeader = ({ onSearch }) => {
+  const inputRef = useRef();
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    console.log(value);
+    onSearch(value);
+  };
+  const onClick = () => {
+    handleSearch();
+  };
+  const onEnter = (e) => {
+    if (e.keyCode === 13) {
+      handleSearch();
+    }
+  };
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <img src="/images/logo.png" alt="" className={styles.img} />
+          <h1 className={styles.title}>Youtube</h1>
+        </div>
+        <input
+          type="search"
+          placeholder="검색어를 입력하세요"
+          className={styles.input}
+          onKeyDown={onEnter}
+          ref={inputRef}
+        />
+        <button type="submit" className={styles.button} onClick={onClick}>
+          <img src="/images/search.png" alt="" className={styles.buttonimg} />
+        </button>
+      </header>
+    </>
+  );
+};
+
+export default SearchHeader;
